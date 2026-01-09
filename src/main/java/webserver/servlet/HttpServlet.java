e9171aad93e6f44326193333b448acecc56bd075
jho7535 <jho7535@naver.com>
2026-01-09 14:17:44 +0900

    feat : 서블릿의 디폴트 메서드가 모델앤뷰를 반환하도록 변경
    - 모델앤뷰는 디스패처 서블릿으로 반환

--- src/main/java/webserver/servlet/HttpServlet.java
+++ src/main/java/webserver/servlet/HttpServlet.java
@@ -2,27 +2,31 @@ package webserver.servlet;
 
 import webserver.model.HttpRequest;
 import webserver.model.HttpResponse;
+import webserver.model.ModelAndView;
 
 public abstract class HttpServlet {
 
-    public void service(HttpRequest request, HttpResponse response) {
-        switch (request.method()) {
+    public ModelAndView service(HttpRequest request, HttpResponse response) {
+        return switch (request.method()) {
             case GET -> doGet(request, response);
             case POST -> doPost(request, response);
             default -> {
                 response.setStatus(405, "Method Not Allowed");
                 response.setBody("<h1>405 Method Not Allowed</h1>".getBytes());
+                yield null;
             }
-        }
+        };
     }
 
-    protected void doGet(HttpRequest request, HttpResponse response) {
+    protected ModelAndView doGet(HttpRequest request, HttpResponse response) {
         response.setStatus(405, "Method Not Allowed");
         response.setBody("<h1>405 Method Not Allowed</h1>".getBytes());
+        return null;
     }
 
-    protected void doPost(HttpRequest request, HttpResponse response) {
+    protected ModelAndView doPost(HttpRequest request, HttpResponse response) {
         response.setStatus(405, "Method Not Allowed");
         response.setBody("<h1>405 Method Not Allowed</h1>".getBytes());
+        return null;
     }
 }
