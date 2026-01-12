e5588b88175d7f7da138e587a1cfa294a3ede96a
jho7535 <jho7535@naver.com>
2026-01-12 16:53:01 +0900

    feat : 로그인 페이지, 회원가입 페이지 처리할 서블릿 생성

---
+++ src/main/java/webserver/servlet/impl/LoginPageServlet.java
@@ -0,0 +1,32 @@
+package webserver.servlet.impl;
+
+import model.User;
+import webserver.SessionManager;
+import webserver.model.HttpRequest;
+import webserver.model.HttpResponse;
+import webserver.model.HttpSession;
+import webserver.model.ModelAndView;
+import webserver.servlet.HttpServlet;
+
+public class LoginPageServlet extends HttpServlet {
+
+    @Override
+    protected ModelAndView doGet(HttpRequest request, HttpResponse response) {
+        ModelAndView mav = new ModelAndView("login/index");
+
+        HttpSession session = SessionManager.getSession(request.getSessionId());
+        User loginUser = null;
+        if (session != null) {
+            loginUser = (User) session.getAttribute("user");
+        }
+
+        if (loginUser != null) {
+            mav.addObject("isLoggedIn", true)
+                    .addObject("userName", loginUser.getName());
+        } else {
+            mav.addObject("isLoggedIn", false);
+        }
+
+        return mav;
+    }
+}
