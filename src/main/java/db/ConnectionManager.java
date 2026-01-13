6ec7950df5460652f032b6770807ae430850b4d1
jho7535 <jho7535@naver.com>
2026-01-13 11:56:02 +0900

    feat : JDBC 커넥션 관리 클래스 생성

---
+++ src/main/java/db/ConnectionManager.java
@@ -0,0 +1,15 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static final String DB_URL = "jdbc:h2:./was_db";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+}
