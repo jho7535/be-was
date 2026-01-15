3a25787a52592adfde9c9a5960af04fa26f093d6
jho7535 <jho7535@naver.com>
2026-01-15 15:57:45 +0900

    refactor : 회원가입 페이지에 모델 주입 삭제

--- src/main/java/webserver/servlet/impl/RegistrationPageServlet.java
+++ src/main/java/webserver/servlet/impl/RegistrationPageServlet.java
@@ -12,21 +12,6 @@ public class RegistrationPageServlet extends HttpServlet {
 
     @Override
     protected ModelAndView doGet(HttpRequest request, HttpResponse response) {
-        ModelAndView mav = new ModelAndView("registration/index");
-
-        HttpSession session = SessionManager.getSession(request.getSessionId());
-        User loginUser = null;
-        if (session != null) {
-            loginUser = (User) session.getAttribute("user");
-        }
-
-        if (loginUser != null) {
-            mav.addObject("isLoggedIn", true)
-                    .addObject("userName", loginUser.getName());
-        } else {
-            mav.addObject("isLoggedIn", false);
-        }
-
-        return mav;
+        return new ModelAndView("registration/index");
     }
 }
